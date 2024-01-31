fd23deaab35bb02fb62bcf33fad81f65cc9b8bb4
acm289 <acm289@cornell.edu>
2024-01-30 23:45:27 -0500

    Initial commit

---
+++ netgen.nix
@@ -0,0 +1,53 @@
+# Copyright 2023 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+{
+  lib,
+  clangStdenv,
+  fetchFromGitHub,
+  tcl,
+  tk,
+  m4,
+  python3,
+}:
+clangStdenv.mkDerivation {
+  name = "netgen";
+  src = fetchFromGitHub {
+    owner = "RTimothyEdwards";
+    repo = "netgen";
+    rev = "93b9cf65778d3094e54f4acf937402b07bdb98b8";
+    sha256 = "09qnf9g4pvk7s71iilwa273c953w5wp4bkqrpmj0psbrk0nhla4k";
+  };
+
+  configureFlags = [
+    "--with-tk=${tk}"
+    "--with-tcl=${tcl}"
+  ];
+
+  NIX_CFLAGS_COMPILE = "-Wno-implicit-function-declaration -Wno-parentheses -Wno-macro-redefined";
+
+  buildInputs = [
+    tcl
+    tk
+    m4
+    python3
+  ];
+
+  meta = with lib; {
+    description = "Complete LVS tool for comparing SPICE or verilog netlists";
+    homepage = "http://opencircuitdesign.com/netgen/";
+    # The code is technically distributed under GPLv1(!!)+
+    license = licenses.gpl2Plus;
+    platforms = platforms.unix;
+  };
+}
