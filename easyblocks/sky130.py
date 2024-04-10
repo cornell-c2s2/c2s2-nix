fd81e9de6b5f4969b620d00c09e9bfec6e9a3b7c
acm289 <acm289@cornell.edu>
2024-04-10 17:36:38 -0400

    Added PDK support

---
+++ easyblocks/sky130.py
@@ -0,0 +1,48 @@
+
+import os
+import shutil
+import stat
+
+from easybuild.framework.easyblock import EasyBlock
+from easybuild.tools.run import run_cmd
+from easybuild.framework.easyconfig import CUSTOM
+
+#=========================================================================
+# Skywater 130
+#=========================================================================
+
+class EB_Sky130(EasyBlock):
+    """
+    Run the Volare command to build in the install directory
+    """
+
+    # Default to building without Python or Ruby - might want to change in future
+    # (not very customizable in general, but works for now)
+
+    @staticmethod
+    def extra_options():
+        extra_vars = {
+            'pdk': ["sky130", "PDK to install", CUSTOM],
+            'commit': ["78b7bc32ddb4b6f14f76883c2e2dc5b5de9d1cbc", 
+                       "Open PDKs commit to use", CUSTOM],
+        }
+        extra_vars = Tarball.extra_options(extra_vars)
+        return extra_vars
+
+    def install_step(self):
+
+        # Use ccache if necessary
+        pdk = self.cfg['pdk']
+        commit = self.cfg['commit']
+
+        bindir = os.path.join(self.installdir, 'bin')
+        install_cmd = f"export PDK_ROOT={bindir} && "
+
+        install_cmd += f"volare enable --pdk {pdk} {commit}"
+        try:
+            os.makedirs(bindir)
+            (out, _) = run_cmd(install_cmd, log_all=True, simple=False)
+        except OSError as err:
+            raise EasyBuildError("Installing Skywater130 in %s failed: %s", bindir, err)
+
+        return out
